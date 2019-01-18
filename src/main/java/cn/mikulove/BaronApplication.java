0e8ed25930a61bfad86b652dfe706cceaeff6c40
ningandyin <ningandyin6978@gmail.com>
2019-01-18 23:24:13 +0800

    init

---
+++ src/main/java/cn/mikulove/BaronApplication.java
@@ -0,0 +1,14 @@
+package cn.mikulove;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaronApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaronApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/cn/mikulove/BaronApplicationTests.java
@@ -0,0 +1,17 @@
+package cn.mikulove;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BaronApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
