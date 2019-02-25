e8c29d7a415262deffcf5f7f4f647bb16efaa601
ningandyin <ningandyin6978@gmail.com>
2019-02-25 23:22:23 +0800

    user

--- src/main/java/cn/mikulove/controller/UserController.java
+++ src/main/java/cn/mikulove/controller/UserController.java
@@ -1,12 +1,19 @@
 package cn.mikulove.controller;
 
+import javax.annotation.Resource;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import cn.mikulove.service.UserService;
+
 @Controller
 public class UserController {
+	
+	@Resource
+	private UserService userService;
 	
 	@ResponseBody
 	@RequestMapping("/users")
