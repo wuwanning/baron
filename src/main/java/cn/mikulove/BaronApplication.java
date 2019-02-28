0074861eed4989e13edfcf176ba265123766e589
ningandyin <ningandyin6978@gmail.com>
2019-03-01 00:07:42 +0800

    rabbitmq

--- src/main/java/cn/mikulove/BaronApplication.java
+++ src/main/java/cn/mikulove/BaronApplication.java
@@ -1,6 +1,7 @@
 package cn.mikulove;
 
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
@@ -8,6 +9,7 @@ import org.springframework.cache.annotation.EnableCaching;
 @SpringBootApplication
 @MapperScan(basePackages = "cn.mikulove.dao")
 @EnableCaching
+@EnableRabbit
 public class BaronApplication {
 
 	public static void main(String[] args) {
