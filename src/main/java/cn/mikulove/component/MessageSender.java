d4d5d1c07fe37e29e7043531a47fa7aa214650fc
miku <miku@DESKTOP-DIIB4VQ>
2019-03-05 16:34:20 +0800

    rabbit

---
+++ src/main/java/cn/mikulove/component/MessageSender.java
@@ -0,0 +1,16 @@
+package cn.mikulove.component;
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MessageSender {
+
+	@Autowired
+	private RabbitTemplate rabbitTemplate;
+
+	public void sendMessage(String message){
+		
+	} 
+}
